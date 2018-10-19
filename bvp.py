1fc741ed3faa338af29fe8a3beace2223064a592
kylrth <kylrth@gmail.com>
2018-10-19 14:50:13 -0600

    started trying to solve with scipy.stats's solve_bvp

---
+++ bvp.py
@@ -0,0 +1,4 @@
+# This module will contain the boundary value solver code, which we can reference in the Jupyter Notebook.
+
+if __name__ == '__main__':
+    print('This is a module for the BVP solver. To run, call one of the functions.')
