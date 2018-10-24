150f70b00672fd2b20051971ba2e6a1230f7ce68
Jason Gardiner <jasongardiner7@gmail.com>
2018-10-24 15:18:23 -0600

    Started to hardcode the midterm problem

--- bvp.py
+++ bvp.py
@@ -6,40 +6,46 @@ from matplotlib import pyplot as plt
 coeffs = []
 
 
-def fun(cf, a, b, deg, ode, bc):
-
+def fun(cf, a, b, dim, deg, ode, bc):
+    cf = cf.reshape((dim,deg+1))
+    cf = np.array(cf)
+    
     # nodes (Chebyshev extrema)
     x = np.cos((np.pi * np.arange(deg + 1)) / deg)
     # print(x)
     # scale to (a, b)
     x = (x * (b - a) + b + a) / 2
-
+    
     # polynomial evaluated at nodes
-    cheb_poly = np.polynomial.chebyshev.Chebyshev(cf, (a, b))
-    p = cheb_poly(x)
+    cheb_poly = [np.polynomial.chebyshev.Chebyshev(coeffs, (a, b)) for coeffs in cf]
+    
+    p = np.array([cheb_poly[i](x) for i in range(dim)])
 
     # polynomial at the end points
-    ya = p[-1]
-    yb = p[0]
+    ya = p[:, -1]
+    yb = p[:, 0]
 
     # coefficients of the derivative of the polynomial
-    cheb_poly_der = cheb_poly.deriv()
+    cheb_poly_der = [cheb_poly[i].deriv() for i in range(dim)]
 
     # derivative of polynomial evaluated at the nodes
-    p_der = cheb_poly_der(x)
+    p_der = np.array([cheb_poly_der[i](x) for i in range(dim)])
 
-    ya_prime = p_der[0]
-    yb_prime = p_der[-1]
+    ya_prime = p_der[:, 0]
+    yb_prime = p_der[:, -1]
 
     # plot it
     dom = np.linspace(a, b, 1000)
     coeffs.append(cf)
 
     # output for fsolve
-    return [
-        *bc([ya, ya_prime], [yb, yb_prime]),  # boundary conditions
-        *(p_der - ode(x, p))  # ODE conditions
+    stuff = [
+        *np.ravel(bc([ya, ya_prime], [yb, yb_prime])),  # boundary conditions
+        *np.ravel((p_der - ode(x, p)))  # ODE conditions
     ]
+    
+    print(stuff)
+    return stuff
 
 
 def our_own_bvp_solve():
@@ -52,17 +58,17 @@ def our_own_bvp_solve():
     # ODE
     f = lambda x, y: np.array([y[0] * (y[0] - 1) + gamma * (y[1] - y[0] * e_minus),
                                y[0] * (-(y[0] - 1) ** 2 / 2 + y[1] - e_minus + gamma * e_minus * (y[0] - 1))])
-    dim = 2
 
     a = -20
     b = 20
-    n = 10
+    n = 40
+    dim = 2
 
-    bc = lambda ya, yb: [yb[0] - np.array([9 / 50, 3 / 5]), yb[1] - "eigenvector"]
+    bc = lambda ya, yb: [ya[0] - np.array([3 / 50, 1]), yb[0] - np.array([9 / 50, 3 / 5]), yb[1] - np.array([-.6, 1])]
 
-    u0 = np.random.rand(n + 1)
+    u0 = np.random.random((n + 1, dim))
 
-    solution = root(lambda u: fun(u, a, b, n, f, bc), u0, method='lm')
+    solution = root(lambda u: fun(u, a, b, dim, n, f, bc), u0, method='lm')
 
     fudge_factor = 1
 
@@ -77,9 +83,3 @@ def our_own_bvp_solve():
     #     plt.plot(dom, np.polynomial.chebyshev.chebval(dom, cf_temp), label=str(i))
     plt.legend()
     plt.show()
-
-
-
-
-if __name__ == '__main__':
-    print('This is a module for the BVP solver. To run, call one of the functions.')
