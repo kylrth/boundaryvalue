6d495de0e2fb86e2492c5b6928e3a7e3d65e3717
kylrth <kylrth@gmail.com>
2018-10-24 13:43:39 -0600

    fixed for 1D systems

--- bvp.py
+++ bvp.py
@@ -10,6 +10,7 @@ def fun(cf, a, b, deg, ode, bc):
 
     # nodes (Chebyshev extrema)
     x = np.cos((np.pi * np.arange(deg + 1)) / deg)
+    # print(x)
     # scale to (a, b)
     x = (x * (b - a) + b + a) / 2
 
@@ -18,8 +19,8 @@ def fun(cf, a, b, deg, ode, bc):
     p = cheb_poly(x)
 
     # polynomial at the end points
-    ya = p[0]
-    yb = p[-1]
+    ya = p[-1]
+    yb = p[0]
 
     # coefficients of the derivative of the polynomial
     cheb_poly_der = cheb_poly.deriv()
@@ -46,7 +47,7 @@ def our_own_bvp_solve():
 
     a = -1
     b = 1
-    n = 1
+    n = 10
 
     bc = lambda ya, yb: [ya[0] - np.exp(-1)]
 
@@ -54,13 +55,15 @@ def our_own_bvp_solve():
 
     solution = root(lambda u: fun(u, a, b, n, f, bc), u0, method='lm')
 
+    fudge_factor = 1
+
     cf = solution.x
     print(solution.success)  # flesh this out
 
     # plotting
     dom = np.linspace(a, b, 1000)
     plt.plot(dom, np.exp(dom), label='$e^x$')
-    plt.plot(dom, np.polynomial.chebyshev.chebval(dom, cf), label='estimate')
+    plt.plot(dom, np.polynomial.chebyshev.chebval(dom, cf) * fudge_factor, label='estimate')
     # for i, cf_temp in enumerate(coeffs):
     #     plt.plot(dom, np.polynomial.chebyshev.chebval(dom, cf_temp), label=str(i))
     plt.legend()
