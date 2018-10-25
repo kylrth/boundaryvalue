9cd0b346e52d91e03d23cb12d3b73e5291d5edb4
kylrth <kylrth@gmail.com>
2018-10-25 17:59:49 -0600

    works with example from SciPy solve_bvp spec

--- bvp.py
+++ bvp.py
@@ -11,7 +11,7 @@ def fun(cf, a, b, dim, deg, ode, bc):
     cf = np.array(cf)
     
     # nodes (Chebyshev extrema)
-    x = np.cos((np.pi * np.arange(deg + 1)) / deg)
+    x = np.array(list(reversed(np.cos((np.pi * np.arange(deg + 1)) / deg))))
     # print(x)
     # scale to (a, b)
     x = (x * (b - a) + b + a) / 2
@@ -22,8 +22,9 @@ def fun(cf, a, b, dim, deg, ode, bc):
     p = np.array([cheb_poly[i](x) for i in range(dim)])
 
     # polynomial at the end points
-    ya = p[:, -1]
-    yb = p[:, 0]
+    ya = p[:, 0]
+    yb = p[:, -1]
+    print(yb)
 
     # coefficients of the derivative of the polynomial
     cheb_poly_der = [cheb_poly[i].deriv() for i in range(dim)]
@@ -31,55 +32,80 @@ def fun(cf, a, b, dim, deg, ode, bc):
     # derivative of polynomial evaluated at the nodes
     p_der = np.array([cheb_poly_der[i](x) for i in range(dim)])
 
-    ya_prime = p_der[:, 0]
-    yb_prime = p_der[:, -1]
-
-    # plot it
-    dom = np.linspace(a, b, 1000)
-    coeffs.append(cf)
+    # ya_prime = p_der[:, 0]
+    # yb_prime = p_der[:, -1]
 
     # output for fsolve
     stuff = [
-        *np.ravel(bc([ya, ya_prime], [yb, yb_prime])),  # boundary conditions
-        *np.ravel((p_der - ode(x, p)))  # ODE conditions
+        *np.ravel(bc(ya, yb)),  # boundary conditions
+        *np.ravel((p_der - ode(x,p)))  # ODE conditions
     ]
-    
-    print(stuff)
+    # print(len(stuff))
     return stuff
 
 
-def our_own_bvp_solve():
-    # constants
-    gamma = 5 / 3
-    v_plus = 3 / 5
-    v_star = gamma / (gamma + 2)
-    e_minus = (gamma + 2) * (v_plus - v_star) / 2 / gamma / (gamma + 1)
+def our_own_bvp_solve(f, a, b, n, dim, bc):
 
-    # ODE
-    f = lambda x, y: np.array([y[0] * (y[0] - 1) + gamma * (y[1] - y[0] * e_minus),
-                               y[0] * (-(y[0] - 1) ** 2 / 2 + y[1] - e_minus + gamma * e_minus * (y[0] - 1))])
+    # u0 = np.random.random((n + 1, dim))
+    u0 = np.zeros((n + 1, dim))
 
-    a = -20
-    b = 20
-    n = 40
+    solution = root(lambda u: fun(u, a, b, dim, n, f, bc), u0, method='lm', tol=1e-10)
+
+    return solution
+
+
+if __name__ == '__main__':
+    # # constants
+    # gamma = 5 / 3
+    # v_plus = 3 / 5
+    # v_star = gamma / (gamma + 2)
+    # e_minus = (gamma + 2) * (v_plus - v_star) / 2 / gamma / (gamma + 1)
+
+    # # ODE
+    # f = lambda x, y: np.array([y[0] * (y[0] - 1) + gamma * (y[1] - y[0] * e_minus),
+    #                            y[0] * (-(y[0] - 1) ** 2 / 2 + y[1] - e_minus + gamma * e_minus * (y[0] - 1))])
+
+    # a = -20
+    # b = 20
+    # n = 40
+    # dim = 2
+
+    # bc = lambda ya, yb: [ya[0] - np.array([3 / 50, 1]), yb[0] - np.array([9 / 50, 3 / 5]), yb[1] - np.array([-.6, 1])]
+
+    f = lambda x, y: np.array([
+        y[1],
+        -np.exp(y[0])
+    ])
+
+    a = 0
+    b = 1
+    n = 10
     dim = 2
 
-    bc = lambda ya, yb: [ya[0] - np.array([3 / 50, 1]), yb[0] - np.array([9 / 50, 3 / 5]), yb[1] - np.array([-.6, 1])]
+    def bc(ya, yb):
+        return np.array([ya[0], yb[0]])
 
-    u0 = np.random.random((n + 1, dim))
+    # f = lambda x, y: y
 
-    solution = root(lambda u: fun(u, a, b, dim, n, f, bc), u0, method='lm')
+    # a = 0
+    # b = 1
+    # n = 10
+    # dim = 1
 
-    fudge_factor = 1
+    # def bc(ya, yb):
+    #     return ya[0] - 1
+
+    solution = our_own_bvp_solve(f, a, b, n, dim, bc)
 
     cf = solution.x
+    print(cf.shape)
+    cf = cf.reshape((dim, cf.size // dim))
+    print(cf.shape)
+
     print(solution.success)  # flesh this out
 
     # plotting
     dom = np.linspace(a, b, 1000)
-    plt.plot(dom, np.exp(dom), label='$e^x$')
-    plt.plot(dom, np.polynomial.chebyshev.chebval(dom, cf) * fudge_factor, label='estimate')
-    # for i, cf_temp in enumerate(coeffs):
-    #     plt.plot(dom, np.polynomial.chebyshev.chebval(dom, cf_temp), label=str(i))
+    plt.plot(dom, np.polynomial.chebyshev.Chebyshev(cf[0], (a, b))(dom), label='estimate')
     plt.legend()
     plt.show()
