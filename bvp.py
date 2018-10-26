63647fe94a892e175bad377e1a7894d86028987e
kylrth <kylrth@gmail.com>
2018-10-25 18:05:45 -0600

    latest try

--- bvp.py
+++ bvp.py
@@ -24,7 +24,6 @@ def fun(cf, a, b, dim, deg, ode, bc):
     # polynomial at the end points
     ya = p[:, 0]
     yb = p[:, -1]
-    print(yb)
 
     # coefficients of the derivative of the polynomial
     cheb_poly_der = [cheb_poly[i].deriv() for i in range(dim)]
@@ -55,35 +54,35 @@ def our_own_bvp_solve(f, a, b, n, dim, bc):
 
 
 if __name__ == '__main__':
-    # # constants
-    # gamma = 5 / 3
-    # v_plus = 3 / 5
-    # v_star = gamma / (gamma + 2)
-    # e_minus = (gamma + 2) * (v_plus - v_star) / 2 / gamma / (gamma + 1)
+    # constants
+    gamma = 5 / 3
+    v_plus = 3 / 5
+    v_star = gamma / (gamma + 2)
+    e_minus = (gamma + 2) * (v_plus - v_star) / 2 / gamma / (gamma + 1)
 
-    # # ODE
-    # f = lambda x, y: np.array([y[0] * (y[0] - 1) + gamma * (y[1] - y[0] * e_minus),
-    #                            y[0] * (-(y[0] - 1) ** 2 / 2 + y[1] - e_minus + gamma * e_minus * (y[0] - 1))])
-
-    # a = -20
-    # b = 20
-    # n = 40
-    # dim = 2
-
-    # bc = lambda ya, yb: [ya[0] - np.array([3 / 50, 1]), yb[0] - np.array([9 / 50, 3 / 5]), yb[1] - np.array([-.6, 1])]
-
-    f = lambda x, y: np.array([
-        y[1],
-        -np.exp(y[0])
-    ])
+    # ODE
+    f = lambda x, y: np.array([y[0] * (y[0] - 1) + gamma * (y[1] - y[0] * e_minus),
+                               y[0] * (-(y[0] - 1) ** 2 / 2 + y[1] - e_minus + gamma * e_minus * (y[0] - 1))])
 
     a = 0
-    b = 1
-    n = 10
+    b = 20
+    n = 40
     dim = 2
 
-    def bc(ya, yb):
-        return np.array([ya[0], yb[0]])
+    bc = lambda ya, yb: [ya[0] - np.array([3 / 50, 1]), yb[0] - np.array([9 / 50, 3 / 5]), yb[1] - np.array([0, 0])]
+
+    # f = lambda x, y: np.array([
+    #     y[1],
+    #     -np.exp(y[0])
+    # ])
+
+    # a = 0
+    # b = 1
+    # n = 10
+    # dim = 2
+
+    # def bc(ya, yb):
+    #     return np.array([ya[0], yb[0]])
 
     # f = lambda x, y: y
 
@@ -98,14 +97,14 @@ if __name__ == '__main__':
     solution = our_own_bvp_solve(f, a, b, n, dim, bc)
 
     cf = solution.x
-    print(cf.shape)
     cf = cf.reshape((dim, cf.size // dim))
-    print(cf.shape)
 
     print(solution.success)  # flesh this out
 
     # plotting
     dom = np.linspace(a, b, 1000)
-    plt.plot(dom, np.polynomial.chebyshev.Chebyshev(cf[0], (a, b))(dom), label='estimate')
+    plt.plot(np.polynomial.chebyshev.Chebyshev(cf[0], (a, b))(dom),
+             np.polynomial.chebyshev.Chebyshev(cf[1], (a, b))(dom),
+             label='estimate')
     plt.legend()
     plt.show()
