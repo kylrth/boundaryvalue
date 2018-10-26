6daf812933924db594109e7207d76d498f279fcf
kylrth <kylrth@gmail.com>
2018-10-26 15:12:49 -0600

    return functions instead of coefficients

--- bvp.py
+++ bvp.py
@@ -1,9 +1,7 @@
-# This module contains the boundary value solver code, which we can reference in the Jupyter Notebook.
+# This module contains the boundary value solver code, which we call in the Jupyter Notebook.
 import numpy as np
 from scipy.optimize import root
 from matplotlib import pyplot as plt
-
-coeffs = []
 
 
 def fun(cf, a, b, dim, deg, ode, bc):
@@ -12,7 +10,6 @@ def fun(cf, a, b, dim, deg, ode, bc):
     
     # nodes (Chebyshev extrema)
     x = np.array(list(reversed(np.cos((np.pi * np.arange(deg + 1)) / deg))))
-    # print(x)
     # scale to (a, b)
     x = (x * (b - a) + b + a) / 2
     
@@ -39,50 +36,56 @@ def fun(cf, a, b, dim, deg, ode, bc):
         *np.ravel(bc(ya, yb)),  # boundary conditions
         *np.ravel((p_der - ode(x,p)))  # ODE conditions
     ]
-    # print(len(stuff))
+    print(stuff)
     return stuff
 
 
-def our_own_bvp_solve(f, a, b, n, dim, bc):
+def our_own_bvp_solve(f, a, b, n, dim, bc, tol=1e-10):
+    """Solves a boundary value problem using Chebyshev colocation. Returns a list of functions that form the solution to
+    the problem."""
 
-    # u0 = np.random.random((n + 1, dim))
-    u0 = np.zeros((n + 1, dim))
+    u0 = np.random.random((n + 1, dim))
 
-    solution = root(lambda u: fun(u, a, b, dim, n, f, bc), u0, method='lm', tol=1e-10)
+    solution = root(lambda u: fun(u, a, b, dim, n, f, bc), u0, method='lm', tol=tol)
+    if not solution.success:
+        print('root finding failed')
 
-    return solution
+    cf = solution.x
+    cf = cf.reshape((dim, cf.size // dim))
+
+    return [np.polynomial.chebyshev.Chebyshev(cf[i], (a, b)) for i in range(dim)]
 
 
 if __name__ == '__main__':
-    # constants
-    gamma = 5 / 3
-    v_plus = 3 / 5
-    v_star = gamma / (gamma + 2)
-    e_minus = (gamma + 2) * (v_plus - v_star) / 2 / gamma / (gamma + 1)
+    # # constants
+    # gamma = 5 / 3
+    # v_plus = 3 / 5
+    # v_star = gamma / (gamma + 2)
+    # e_minus = (gamma + 2) * (v_plus - v_star) / 2 / gamma / (gamma + 1)
 
-    # ODE
-    f = lambda x, y: np.array([y[0] * (y[0] - 1) + gamma * (y[1] - y[0] * e_minus),
-                               y[0] * (-(y[0] - 1) ** 2 / 2 + y[1] - e_minus + gamma * e_minus * (y[0] - 1))])
-
-    a = 0
-    b = 20
-    n = 40
-    dim = 2
-
-    bc = lambda ya, yb: [ya[0] - np.array([3 / 50, 1]), yb[0] - np.array([9 / 50, 3 / 5]), yb[1] - np.array([0, 0])]
-
-    # f = lambda x, y: np.array([
-    #     y[1],
-    #     -np.exp(y[0])
-    # ])
+    # # ODE
+    # f = lambda x, y: np.array([y[0] * (y[0] - 1) + gamma * (y[1] - y[0] * e_minus),
+    #                            y[0] * (-(y[0] - 1) ** 2 / 2 + y[1] - e_minus + gamma * e_minus * (y[0] - 1))])
 
     # a = 0
-    # b = 1
-    # n = 10
+    # b = 20
+    # n = 40
     # dim = 2
 
-    # def bc(ya, yb):
-    #     return np.array([ya[0], yb[0]])
+    # bc = lambda ya, yb: [ya[0] - np.array([3 / 50, 1]), yb[0] - np.array([9 / 50, 3 / 5]), yb[1] - np.array([0, 0])]
+
+    f = lambda x, y: np.array([
+        y[1],
+        -np.exp(y[0])
+    ])
+
+    a = 0
+    b = 1
+    n = 20
+    dim = 2
+
+    def bc(ya, yb):
+        return np.array([ya[0], yb[0]])
 
     # f = lambda x, y: y
 
@@ -96,15 +99,10 @@ if __name__ == '__main__':
 
     solution = our_own_bvp_solve(f, a, b, n, dim, bc)
 
-    cf = solution.x
-    cf = cf.reshape((dim, cf.size // dim))
-
-    print(solution.success)  # flesh this out
-
     # plotting
     dom = np.linspace(a, b, 1000)
-    plt.plot(np.polynomial.chebyshev.Chebyshev(cf[0], (a, b))(dom),
-             np.polynomial.chebyshev.Chebyshev(cf[1], (a, b))(dom),
+    plt.plot(dom, solution[0](dom),
+            #  np.polynomial.chebyshev.Chebyshev(cf[1], (a, b))(dom),
              label='estimate')
     plt.legend()
     plt.show()
