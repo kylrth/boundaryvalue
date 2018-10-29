1f1b6e3ca7fc230700dc2c609d07b9fea324b495
kylrth <kylrth@gmail.com>
2018-10-29 16:12:29 -0600

    updates in preparation to ask Dr. Barker what's up

--- bvp.py
+++ bvp.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 
 
 def fun(cf, a, b, dim, deg, ode, bc):
-    cf = cf.reshape((dim,deg+1))
+    cf = cf.reshape((dim, deg+1))
     cf = np.array(cf)
     
     # nodes (Chebyshev extrema)
@@ -32,24 +32,19 @@ def fun(cf, a, b, dim, deg, ode, bc):
     # yb_prime = p_der[:, -1]
 
     # output for fsolve
-    stuff = [
+    return [
         *np.ravel(bc(ya, yb)),  # boundary conditions
         *np.ravel((p_der - ode(x,p)))  # ODE conditions
     ]
-    print(stuff)
-    return stuff
 
 
-def our_own_bvp_solve(f, a, b, n, u0, dim, bc, tol=1e-10):
+def our_own_bvp_solve(f, a, b, n, y0, dim, bc, tol=1e-10):
     """Solves a boundary value problem using Chebyshev colocation. Returns a list of functions that form the solution to
     the problem."""
 
-    y0 = np.polynomial.chebyshev.chebfit(np.linspace(a, b, len(u0)), u0, 20)
-    print(y0)
-    plt.plot()
-    input()
+    cf0 = np.polynomial.chebyshev.chebfit(np.linspace(a, b, len(y0)), y0, n)
 
-    solution = root(lambda u: fun(u, a, b, dim, n, f, bc), y0, method='lm', tol=tol)
+    solution = root(lambda u: fun(u, a, b, dim, n, f, bc), cf0, method='lm', tol=tol)
     if not solution.success:
         print('root finding failed')
 
@@ -60,56 +55,45 @@ def our_own_bvp_solve(f, a, b, n, u0, dim, bc, tol=1e-10):
 
 
 if __name__ == '__main__':
-    # # constants
-    # gamma = 5 / 3
-    # v_plus = 3 / 5
-    # v_star = gamma / (gamma + 2)
-    # e_minus = (gamma + 2) * (v_plus - v_star) / 2 / gamma / (gamma + 1)
-
-    # # ODE
-    # f = lambda x, y: np.array([y[0] * (y[0] - 1) + gamma * (y[1] - y[0] * e_minus),
-    #                            y[0] * (-(y[0] - 1) ** 2 / 2 + y[1] - e_minus + gamma * e_minus * (y[0] - 1))])
-
-    # a = 0
-    # b = 20
-    # n = 40
-    # dim = 2
-
-    # bc = lambda ya, yb: [ya[0] - np.array([3 / 50, 1]), yb[0] - np.array([9 / 50, 3 / 5]), yb[1] - np.array([0, 0])]
-
-    f = lambda x, y: np.array([
-        y[1],
-        -np.exp(y[0])
-    ])
+    # The following is the system for the midterm.
+    gamma = 5 / 3
+    v_plus = 3 / 5
+    v_star = gamma / (gamma + 2)
+    e_minus = (gamma + 2) * (v_plus - v_star) / 2 / gamma / (gamma + 1)
+    # ODE
+    f = lambda x, y: np.array([y[0] * (y[0] - 1) + gamma * (y[1] - y[0] * e_minus),
+                               y[0] * (-(y[0] - 1) ** 2 / 2 + y[1] - e_minus + gamma * e_minus * (y[0] - 1))])
 
     a = 0
-    b = 1
-    n = 20
+    b = 20
+    n = 40
     dim = 2
 
-    def bc(ya, yb):
-        return np.array([ya[0], yb[0]])
+    bc = lambda ya, yb: [ya[0] - np.array([3 / 50, 1]), yb[0] - np.array([9 / 50, 3 / 5]), yb[1] - np.array([0, 0])]
 
-    # f = lambda x, y: y
+    # # The following is the first example system from
+    # # https://docs.scipy.org/doc/scipy-0.18.1/reference/generated/scipy.integrate.solve_bvp.html.
+    # f = lambda x, y: np.array([
+    #     y[1],
+    #     -np.exp(y[0])
+    # ])
 
     # a = 0
     # b = 1
-    # n = 10
-    # dim = 1
+    # n = 20
+    # dim = 2
 
     # def bc(ya, yb):
-    #     return ya[0] - 1
+    #     return np.array([ya[0], yb[0]])
 
-    u0 = np.zeros((n + 1, dim))
-    u0[0, 0] = 3
-    u0[:, 1] = np.exp(u0[:, 0])
+    y0 = np.zeros((n + 1, dim))
+    # y0[0, 0] = 3
+    # y0[:, 1] = np.exp(y0[:, 0])
 
-    solution = our_own_bvp_solve(f, a, b, n, u0, dim, bc)
+    solution = our_own_bvp_solve(f, a, b, n, y0, dim, bc)
 
     # plotting
     dom = np.linspace(a, b, 1000)
-    plt.plot(dom, solution[0](dom),
-            #  np.polynomial.chebyshev.Chebyshev(cf[1], (a, b))(dom),
-             label='estimate')
+    plt.plot(solution[0](dom), solution[1](dom), label='estimate')
     plt.legend()
     plt.show()
